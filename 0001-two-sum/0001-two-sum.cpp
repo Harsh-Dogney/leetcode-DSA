738c175a605b08330fbba1fd9a5b72c2646f760c
Harsh Dogney <116560167+Harsh-Dogney@users.noreply.github.com>
2024-09-20 18:08:31 +0530

    Time: 10 ms (64.88%), Space: 14.4 MB (37.86%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int ,int >mp;
+        int n = size(nums);
+        for(int i =0;i<n;i++){
+            int a = nums[i];
+            int more = target - a;
+            if(mp.find(more)!=mp.end()){
+                return {mp[more],i};
+            }
+            mp[a]=i;     
+         }
+         return {-1,-1};
+    }
+};
