919f4ee687e8121853cee3b5ae549298bf8b14e2
Harsh Dogney <116560167+Harsh-Dogney@users.noreply.github.com>
2024-09-28 10:09:24 +0530

    Time: 3 ms (85.85%), Space: 19.6 MB (84.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,25 +11,44 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    ListNode* dummy = new ListNode(-1);
-    ListNode* temp=dummy;
-    ListNode* t1=list1;
-    ListNode* t2=list2;
+    // ListNode* dummy = new ListNode(-1);
+    // ListNode* temp=dummy;
+    // ListNode* t1=list1;
+    // ListNode* t2=list2;
     
-    while(t1!=NULL && t2!=NULL){
-        if(t1->val < t2->val ){
-            temp->next=t1;
-            temp=t1;
-            t1=t1->next;
+    // while(t1!=NULL && t2!=NULL){
+    //     if(t1->val < t2->val ){
+    //         temp->next=t1;
+    //         temp=t1;
+    //         t1=t1->next;
+    //     }
+    //     else{
+    //         temp->next=t2;
+    //         temp=t2;
+    //         t2=t2->next;
+    //     }
+    // }
+    // if(t1 != nullptr)temp->next=t1;
+    // else temp->next=t2;
+    // return dummy->next;   
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
+
+        while (list1 && list2) {
+            if (list1->val > list2->val) {
+                cur->next = list2;
+                list2 = list2->next;
+            } else {
+                cur->next = list1;
+                list1 = list1->next;
+            }
+            cur = cur->next;
         }
-        else{
-            temp->next=t2;
-            temp=t2;
-            t2=t2->next;
-        }
-    }
-    if(t1 != nullptr)temp->next=t1;
-    else temp->next=t2;
-    return dummy->next;   
+
+        cur->next = list1 ? list1 : list2;
+
+        return dummy->next;        
+ 
     }
 };
