35e0c6c11bcb4d7baa56ea147d60f87668e69752
Harsh Dogney <116560167+Harsh-Dogney@users.noreply.github.com>
2024-12-26 11:58:49 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    int i = m-1;
-        int j = n-1;
-        int k = m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i] > nums2[j]){
-                nums1[k] = nums1[i];
-                i--; k--;
-            }
-            else{
-                nums1[k] = nums2[j];
-                j--; k--;
+        int arr3[m + n];
+        int l = 0;
+        int r = 0;
+        int index = 0;
+
+        while (l < m && r < n) {
+            if (nums1[l] < nums2[r]) {
+                arr3[index] = nums1[l];
+                l++;
+                index++;
+            } else {
+                arr3[index] = nums2[r];
+                r++, index++;
             }
         }
-        while(j>=0){
-            nums1[k] = nums2[j];
-            j--; k--;
+
+    while (l < m) {
+            arr3[index++] = nums1[l++];
+        }
+
+        // Copy remaining elements of nums2, if any
+        while (r < n) {
+            arr3[index++] = nums2[r++];
+        }
+
+        // Copy merged array back into nums1
+        for (int i = 0; i < m + n; i++) {
+            nums1[i] = arr3[i];
         }
     }
 };
