ed33d7b4ae6e936033aac84559a13a2064a1319f
Harsh Dogney <116560167+Harsh-Dogney@users.noreply.github.com>
2024-12-30 21:51:08 +0530

    Time: 12 ms (35.92%), Space: 11.8 MB (49.36%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,24 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-    // map<ListNode* ,int >mp;
-    // ListNode *temp =head;
-    // while(temp!=NULL){
-    //     if (mp.find(temp) != mp.end()) {
-    // return true;}
+    ListNode *slow= head;
+    ListNode *fast = head;
 
-    //     mp[temp]=1;
-    //     temp=temp->next;
-    // }
-    
-    // return false;
-    ListNode *slow = head;
-ListNode *fast = head;
-while (fast && fast->next) {
-    slow = slow->next;
-    fast = fast->next->next;
-    if (slow == fast) return true;  // Cycle detected
-}
-return false;  // No cycle
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)return true ;
+       
+    }
+    return false;
     }
 };
