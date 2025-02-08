1af7051463ca54c9b4aea9ffe4fbc107bcea1425
Harsh Dogney <116560167+Harsh-Dogney@users.noreply.github.com>
2025-02-08 16:41:49 +0530

    Time: 0 ms (100%), Space: 29.4 MB (97.31%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
     k %=nums.size();
-    reverse(begin(nums),end(nums));
-    reverse(nums.begin(),nums.begin()+k);
-    reverse(nums.begin()+k,nums.end());  
+     reverse(begin(nums), end(nums)); 
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+   
     }
 };
+
