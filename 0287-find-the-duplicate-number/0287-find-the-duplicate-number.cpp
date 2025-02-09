4bea1b7c4dea6efe18ac02e6a4052d0c67f90d50
Harsh Dogney <116560167+Harsh-Dogney@users.noreply.github.com>
2025-02-10 00:02:49 +0530

    Time: 109 ms (10.82%), Space: 104.8 MB (5.58%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+    int ans=0;
+    int n=nums.size();
+    unordered_map<int,int>mp;
+    for(int i=0;i<n;i++){
+        mp[nums[i]]++;
+    }   
+    for(auto i: mp){
+        if(i.second > 1) ans= i.first;
+    }
+    return ans;
+    }
+};
