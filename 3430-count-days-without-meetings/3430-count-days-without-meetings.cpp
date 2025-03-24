fff4ca4681c92fe3f52b61abfdf89316de8fc6a5
Harsh Dogney <116560167+Harsh-Dogney@users.noreply.github.com>
2025-03-24 12:41:45 +0530

    Time: 68 ms (64.63%), Space: 125.7 MB (99.49%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings){
+        sort(meetings.begin(), meetings.end());
+
+        long long totalMeetingDays = 0;
+        int start = meetings[0][0];
+        int end = meetings[0][1];
+
+        for (int i = 1; i < meetings.size(); i++) {
+            if (meetings[i][0] <= end) {
+
+                end = max(end, meetings[i][1]);
+            } else {
+
+                totalMeetingDays += (end - start + 1);
+                start = meetings[i][0];
+                end = meetings[i][1];
+            }
+        }
+
+        totalMeetingDays += (end - start + 1);
+
+
+        return days - totalMeetingDays;
+    }
+};
+static const int speedup=[](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
